e65e7d90b918a8403842653d3aff65f62878dc98
htoohtetaung1 <htoohtetaung1@gmail.com>
2024-01-26 10:13:48 +0630

    Rewrite code to console based interaction
    Deleted prompts and alerts because it is causing problems. Added methods
    to get player choice and computer choice.

--- script.js
+++ script.js
@@ -1,61 +1,35 @@
-game();
-
-
-function game() {
-    let computerWins = 0;
-    let playerWins = 0;
-    
-}
-
-
-function playRound(playerChoice,computerChoice) {
-    
-    if (playerChoice == "rock" && computerChoice == "rock") {
-        alert("It's a tie! Play again.");
-    } else if (playerChoice == "rock" && computerChoice == "paper") {
-        alert("You lose!");
-        computerWins++;
-        alert("Your wins: " + playerWins + "\nComputer wins: " + computerWins);
-    } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        alert("You win!");
-        playerWins++;
-        alert("Your wins: " + playerWins + "\nComputer wins: " + computerWins);
-    }
-}
-
-function getPlayerChoice() {
-    let playerChoice = (prompt("Rock, Paper, or Scissors?")).toLowerCase();
-    alert(playerChoice);
-    if (checkPlayerChoice(playerChoice)) {
-        return playerChoice;
-    }else {
-        getPlayerChoice();
-    }
-}
-
-function checkPlayerChoice(choice) {
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return true;
-    } else {
-        alert("Incorrect choice. Try again.")
-        return false;
-    }
-}
-
 function getComputerChoice() {
-    let computerChoiceInt = Math.floor(Math.random()*3 +1);
-    let computerChoice;
-    switch (computerChoiceInt) {
-        case 1: 
-            computerChoice = "rock"; 
-            break;
-        case 2:
-            computerChoice = "paper";
-            break;
-        case 3:
-            computerChoice = "scissors";
-            break;
-    }
-    return computerChoice;
+  let computerChoice = Math.floor(Math.random() * 3 + 1);
+  switch (computerChoice) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
 }
-
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, Paper or Scissors?", "rock");
+  if (playerChoice !== null) {
+    playerChoice = playerChoice.toLowerCase();
+  } 
+  if (
+    playerChoice == "rock" ||
+    playerChoice == "paper" ||
+    playerChoice == "scissors"
+    ) {
+      return playerChoice;
+    } else {
+      console.log("Invalid choice try again");
+      getPlayerChoice();
+    }
+  }
+  
+  console.log("Player Choice = " + getPlayerChoice());
+  console.log("Computer choice = " + getComputerChoice());
+  
