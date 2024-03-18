ff8b6e33dba86afe0975f0d6ca3bcfb8f2b11525
htoohtetaung1 <htoohtetaung1@gmail.com>
2024-03-18 13:39:20 +0630

    Add user interface to the game
    Added user interface with buttons and DOM manipulation using javascript.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let playerWins = 0;
+let computerWins = 0;
+let buttons = document.querySelectorAll(".playerChoice");
+let playerChoice, computerChoice;
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playerChoice = e.target.id;
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+  });
+});
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3 + 1);
   switch (computerChoice) {
@@ -13,71 +25,60 @@ function getComputerChoice() {
   }
   return computerChoice;
 }
-function getPlayerChoice() {
-  let playerChoice = prompt("Rock, Paper or Scissors?", "rock");
-  while (!playerChoiceIsCorrect(playerChoice)) {
-    playerChoice = prompt("Rock, Paper or Scissors?", "rock");
-  }
-  return playerChoice.toLowerCase();
-}
-function playerChoiceIsCorrect(choice) {
-  if (choice !== null) {
-    choice = choice.toLowerCase();
-  }
-  if (choice == "rock" || choice == "paper" || choice == "scissors") {
-    return true;
-  } else {
-    console.log("Invalid choice try again");
-    return false;
-  }
-}
-let playerWins = 0;
-let computerWins = 0;
 
+resultsText = document.querySelector(".results");
 function playRound(pChoice, cChoice) {
+  resultsText.innerText =
+    "Your choice = " + pChoice + "\n" + "Computer choice = " + cChoice;
+  let text = document.createElement("div");
   if (
     (pChoice == "rock" && cChoice == "rock") ||
     (pChoice == "paper" && cChoice == "paper") ||
     (pChoice == "scissors" && cChoice == "scissors")
   ) {
-    console.log("It's a draw. Game is not counted.");
+    text.innerText = "It's a draw. Game is not counted.";
   } else if (
     (pChoice == "rock" && cChoice == "paper") ||
     (pChoice == "paper" && cChoice == "scissors") ||
     (pChoice == "scissors" && cChoice == "rock")
   ) {
-    console.log("You Lose!");
     computerWins++;
-    console.log(
-      "Your Wins = " + playerWins + ". Computer Wins = " + computerWins + "\n "
-    );
+    text.innerText =
+      "You lose this round.\nYour Wins = " +
+      playerWins +
+      ". Computer Wins = " +
+      computerWins;
   } else if (
     (pChoice == "rock" && cChoice == "scissors") ||
     (pChoice == "paper" && cChoice == "rock") ||
     (pChoice == "scissors" && cChoice == "paper")
   ) {
-    console.log("You Win!");
     playerWins++;
-    console.log(
-      "Your Wins = " + playerWins + ". Computer Wins = " + computerWins + "\n "
-    );
+    text.innerText =
+      "You win this round.\nYour Wins = " +
+      playerWins +
+      ". Computer Wins = " +
+      computerWins;
   }
-}
-gameOver = false;
-console.log("Best of 5!");
-while (!gameOver) {
-  let computerChoice = getComputerChoice();
-  let playerChoice = getPlayerChoice();
-  console.log(
-    "Player Choice = " + playerChoice + ". Computer Choice = " + computerChoice
-  );
-  playRound(playerChoice, computerChoice);
-  if(computerWins == 3) {
-    console.log("Game over, you lost.");
-    gameOver = true;
+  text.style.color = "red";
+  text.style.fontWeight = "bold"
+
+  resultsText.appendChild(text);
+  if (computerWins == 3) {
+    text.innerText += "\nGame Over!! You lose!!";
+    resetGame(resultsText);
   }
   if (playerWins == 3) {
-    console.log("Congrats! You won the game!")
-    gameOver = true;
+    text.innerText += "\nGame Over!! You Win!!";
+    resetGame(resultsText);
   }
+}
+
+function resetGame(resultsText) {
+  playerWins = 0;
+  computerWins = 0;
+  let resetButton = document.createElement("button")
+  resetButton.textContent = "reset"
+  resultsText.appendChild(resetButton);
+  buttons.disabled = true;
 }
